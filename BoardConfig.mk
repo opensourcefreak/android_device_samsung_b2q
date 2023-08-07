0891575c10efff7d047d6ba70f3105f82d32c524
LucasBlackLu <lucasblack997@gmail.com>
2023-08-08 02:54:54 +0800

    t2q: Use prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,15 +38,15 @@ TARGET_SCREEN_DENSITY := 450
 BUILD_BROKEN_DUP_RULES := true
 
 # Kernel
-BOARD_BOOTIMG_HEADER_VERSION := 3
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := t2q_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/t2q
+#BOARD_BOOTIMG_HEADER_VERSION := 3
+#BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4
+#BOARD_KERNEL_PAGESIZE := 4096
+#BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+#BOARD_KERNEL_IMAGE_NAME := Image
+#BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+#BOARD_KERNEL_SEPARATED_DTBO := true
+#TARGET_KERNEL_CONFIG := t2q_defconfig
+#TARGET_KERNEL_SOURCE := kernel/samsung/t2q
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
